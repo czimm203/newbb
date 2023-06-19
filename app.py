8ed9fe99fe60f0d598aeb8c4aeee0dc5fec33f9f
Cole Zimmerman <colezimmerman@Coles-MacBook-Pro.local>
2023-06-18 22:33:07 -0500

    building forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/report/")
+def report():
+    return render_template("report.html")
+
+@app.route("/report/submit", methods = ["POST"])
+def submit_report():
+    print(request.form)
+    return render_template("submission.html")
